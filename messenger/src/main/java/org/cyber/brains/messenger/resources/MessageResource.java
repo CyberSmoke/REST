45eb89d12f57f4506e7a920954e10d0e4a2bba7a
CyberSmoke <mannurvr@nih.gov>
2017-07-12 11:14:36 -0400

    Initial commit with Model, resource and service classes

---
+++ messenger/src/main/java/org/cyber/brains/messenger/resources/MessageResource.java
@@ -0,0 +1,24 @@
+package org.cyber.brains.messenger.resources;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.cyber.brains.messenger.model.Message;
+import org.cyber.brains.messenger.service.MessageService;
+
+@Path("/messages")
+public class MessageResource 
+{
+	MessageService messageService = new MessageService();
+	
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Message> getMessages()
+	{
+		return messageService.getAllMessages();
+	}
+}
---
+++ messenger/src/main/java/org/cyber/brains/messenger/service/MessageService.java
@@ -0,0 +1,48 @@
+package org.cyber.brains.messenger.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.cyber.brains.messenger.database.DatabaseClass;
+import org.cyber.brains.messenger.model.Message;
+
+public class MessageService 
+{
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
+	
+	public List<Message> getAllMessages()
+	{
+		return new ArrayList<Message>( messages.values() );		
+	}
+	
+	public Message getMessage( long id )
+	{
+		return messages.get( id );
+	}
+	
+	public Message addMessage( Message message )
+	{
+		message.setId( messages.size() + 1 );
+		messages.put(message.getId(), message);
+		
+		return message;
+	}
+	
+	public Message updateMessage( Message message )
+	{
+		if( message.getId() <= 0 )
+		{
+			return null;
+		}
+		
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message deleteMessage( long id )
+	{
+		return messages.remove( id );
+	}
+}
