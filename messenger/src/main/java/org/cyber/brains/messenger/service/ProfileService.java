d6b088f02c56d92e40c4ec9421ba3d072c22311d
CyberSmoke <mannurvr@nih.gov>
2017-07-14 10:26:41 -0400

    Adding Profile Resource

---
+++ messenger/src/main/java/org/cyber/brains/messenger/service/ProfileService.java
@@ -0,0 +1,51 @@
+package org.cyber.brains.messenger.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.cyber.brains.messenger.database.DatabaseClass;
+import org.cyber.brains.messenger.model.Profile;
+
+public class ProfileService 
+{
+	private Map<String, Profile> profiles = DatabaseClass.getProfiles();
+	
+	public ProfileService()
+	{
+		profiles.put("Cyber", new Profile(1L, "Cyber", firstName, lastName))
+	}
+	
+	public Profile addProfile( Profile profile )
+	{
+		profile.setId( profiles.size() + 1 );
+		profiles.put( profile.getProfileName(), profile);
+		return profile;
+	}
+	
+	public void removeProfile( String profileName )
+	{
+		profiles.remove( profileName );
+	}
+	
+	public List<Profile> getAllProfiles()
+	{
+		return new ArrayList<Profile>( profiles.values() );
+	}
+	
+	public Profile getProfile( String profileName )
+	{
+		return profiles.get(profileName);
+	}
+	
+	public Profile updateProfile( Profile profile )
+	{
+		if( profile.getProfileName() == null )
+		{
+			return null;
+		}
+		
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+}
